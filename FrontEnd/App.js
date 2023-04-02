4ff973a89e75cd594168fb51d35053e5c121fe9e
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-04-02 23:04:45 +0900

    init repo

---
+++ FrontEnd/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.title}>sdf</Text>
+        <Text style={styles.title}>김하진 바보</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "pink",
+    fontSize: 40,
+  },
+});
