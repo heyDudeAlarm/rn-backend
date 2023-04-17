82d50cd02729cbe8119d1ac6ebcf5d4fd26f78f0
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-04-18 02:51:54 +0900

    Style: login UI

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,26 +1,73 @@
 import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import axios from "axios";
 
 export default function App() {
   const [data, setData] = useState(null);
-  useEffect(() => {
-    axios
-      .get("http://10.96.123.85:3000/api/data")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://10.96.123.85:3000/api/data")
+  //     .then((response) => {
+  //       setData(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
+  const GradientButton = ({ onPress, style, colors, text }) => {
+    return (
+      <TouchableOpacity onPress={onPress}>
+        <LinearGradient colors={colors} style={style}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>{text}</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.title}>
-        <Text>{JSON.stringify(data)}</Text>
-        <Text style={styles.title}>김하진 바보</Text>
+        <Text
+          style={{
+            color: "#8C92FF",
+            fontSize: 40,
+            fontWeight: "bold",
+          }}
+        >
+          Hey Dude!
+        </Text>
+        {/* <Text>{JSON.stringify(data["message"])}</Text> */}
+      </View>
+      <View>
+        <TextInput style={styles.InputStyle} placeholder="id" />
+        <TextInput style={styles.InputStyle} placeholder="password" />
+      </View>
+      <View>
+        <GradientButton
+          onPress={() => alert("Button Pressed")}
+          style={styles.fillButton}
+          colors={["#8C92FF", "#92FBE7"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0.5, y: 0.5 }}
+          text="Login"
+        />
+        <GradientButton
+          onPress={() => alert("Button Pressed")}
+          style={styles.borderButton}
+          colors={["#8C92FF", "#92FBE7"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0.5, y: 0.5 }}
+          text="Login"
+        />
       </View>
     </View>
   );
@@ -29,13 +76,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    rowGap: 40,
   },
   title: {
     color: "pink",
     fontSize: 40,
+  },
+  InputStyle: {
+    backgroundColor: "#EDEFF2",
+    borderRadius: 10,
+    width: 300,
+    height: 58,
+    padding: 15,
+    margin: 10,
+  },
+  fillButton: {
+    width: 300,
+    height: 58,
+    textAlign: "center",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  borderButton: {
+    width: 300,
+    height: 58,
+    textAlign: "center",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
   },
 });
