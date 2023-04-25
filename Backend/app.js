114190fa554b32a2b73b0fe20cce7c933f9ae7e6
HA JIN <w2128@e-mirim.hs.kr>
2023-04-25 10:20:17 +0900

    controllers 디렉토리 생성

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,11 @@ const path = require('path');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const passport = require('passport');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 
 app.set('port', process.env.port || 8080);
+app.use()
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
+
+router.post()
