14c40d86796168a9bd02e1e201bb0e14e8f03ca8
HA JIN <w2128@e-mirim.hs.kr>
2023-04-25 23:42:16 +0900

    sequelize로 db 연결

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,19 @@ const cors = require('cors');
 
 dotenv.config();
 
+const { sequelize } = require('./models');
+const exp = require('constants');
+
 const app = express();
 
 app.set('port', process.env.port || 8080);
-app.use()
+
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+app.use(express.urlencoded({extended: false}));
