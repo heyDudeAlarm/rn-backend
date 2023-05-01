b4b1292d928195c53bc34097efb29827a773b54f
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-05-01 15:14:32 +0900

    Design: gradient-border-button

--- FrontEnd/src/screens/Login.js
+++ FrontEnd/src/screens/Login.js
@@ -11,23 +11,46 @@ import {
 import { LinearGradient } from "expo-linear-gradient";
 // import axios from "axios";
 
-export default function Login({ navigation }) {
-  //   const [data, setData] = useState(null);
-  //   useEffect(() => {
-  //     axios
-  //       .get("http://10.96.123.85:3000/api/data")
-  //       .then((response) => {
-  //         setData(response.data);
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       });
-  //   }, []);
-  const GradientButton = ({ onPress, style, colors, text }) => {
+export default function Login() {
+  // const [data, setData] = useState(null);
+  // useEffect(() => {
+  //   axios
+  //     .get(`http://10.96.123.85:3000/api/data`)
+  //     .then((response) => {
+  //       setData(response.data); // data state를 node.js 받아온 json으로 설정함.
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
+
+  const GradientButton = ({ onPress, colors, text, textStyle }) => {
     return (
       <TouchableOpacity onPress={onPress}>
-        <LinearGradient colors={colors} style={style}>
-          <Text style={{ color: "white", fontWeight: "bold" }}>{text}</Text>
+        <LinearGradient
+          colors={colors}
+          style={styles.fillButton}
+          start={{ x: 0.0, y: 1.0 }}
+          end={{ x: 1.0, y: 1.0 }}
+        >
+          <Text style={textStyle}>{text}</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    );
+  };
+
+  const GradientBorderButton = ({ onPress, colors, text, textStyle }) => {
+    return (
+      <TouchableOpacity onPress={onPress}>
+        <LinearGradient
+          start={[0, 0.5]}
+          end={[1, 0.5]}
+          colors={colors}
+          style={styles.borderButton}
+        >
+          <View style={styles.circleGradient}>
+            <Text style={styles.visit}>{text}</Text>
+          </View>
         </LinearGradient>
       </TouchableOpacity>
     );
@@ -59,20 +82,18 @@ export default function Login({ navigation }) {
       </View>
       <View>
         <GradientButton
-          onPress={() => alert("Button Pressed")}
+          onPress={() => alert("로그인 되었습니다.")}
           style={styles.fillButton}
           colors={["#8C92FF", "#92FBE7"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0.5, y: 0.5 }}
           text="Login"
+          textStyle={styles.buttonText}
         />
-        <GradientButton
+        <GradientBorderButton
           onPress={() => alert("Button Pressed")}
-          style={styles.borderButton}
+          style={styles.visit}
+          style2={styles.circleGradient}
           colors={["#8C92FF", "#92FBE7"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0.5, y: 0.5 }}
-          text="Sign Up!"
+          text="Sign Up"
         />
       </View>
     </View>
@@ -116,5 +137,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 15,
+    margin: 1,
+    backgroundColor: "white",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#ffffff",
+    padding: 15,
+    marginLeft: 1,
+    marginRight: 1,
+    width: 198,
+    fontSize: 16,
+  },
+  circleGradient: {
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  visit: {
+    margin: 14.5,
+    paddingHorizontal: 104,
+    textAlign: "center",
+    backgroundColor: "white",
+    color: "#4C64FF",
+    fontSize: 16,
   },
 });
