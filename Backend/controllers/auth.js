83992eeb0d282620b6e17b169695b298a47f2c89
HA JIN <w2128@e-mirim.hs.kr>
2023-06-03 13:07:21 +0900

    body 파싱 완료 -> /auth 최종 완료

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,7 +1,5 @@
 //회원가입, 로그인, 로그아웃
 const crypto = require('crypto');
-// const { sequelize } = require('sequelize');
-// const User = require('../models/user');
 const mariaDB = require('../database/config');
 
 const hash = (password) => {
@@ -11,53 +9,54 @@ const hash = (password) => {
     return crypto.createHash("sha256").update(password).digest('base64').toString();
 }
 //회원가입 컨트롤러
-// module.exports.join = async (req, res, next) => {
-//     try {
-//         let [inputEmail, inputNickname, inputPW, pwChk] = req.body;
-//         if(inputPW == pwChk){
-//             await User.findAll({ where: {inputEmail}})
-//             .then(row => { //findAll에서 중복된 email이 있다면
-//                 const result = {message: 'user exist', row: row};
-//                 return res.json(result); 
-//             }).catch(() => { //DB에서 email이 중복되지 않는다면 회원가입
-//                 User.create({
-//                     email: inputEmail,
-//                     nickname: inputNickname,
-//                     password: hash(inputPW),
-//                 }).then(row => {
-//                     const result = {success: true, message: "new user insert!!", row: row};
-//                     return res.json(result);
-//                 }).catch(err => {
-//                     return res.json(err);
-//                 })
-//             })
-//         }
-        
-//     } catch(err) {
-//         console.error('MariaDB 연결 실패');
-//         const result = { success: false, message: err };
-//         console.log(err)
-//         return res.json(result);
-//     }
-// }
+module.exports.join = async (req, res, next) => {
+    try {
+        let conn = await mariaDB.getConnection();
+        const email = req.body.email;
+        const password = req.body.password;
+        const nickname = req.body.nickname;
+
+        let sql = `INSERT INTO users(email, nickname, password) VALUES(
+            '${email}', '${nickname}', '${hash(password)}'
+           );`;
+        await conn.query(`SELECT * FROM users WHERE email = '${email}'`)
+            .then(row => {
+                if(row == 0){
+                    conn.query(sql)
+                        .then(row => {
+                            res.end('회원가입 성공');
+                    })
+                } else {
+                    res.json({fail: '계정이 이미 존재함'});
+                }
+            }).catch(err => {
+                console.log(err);
+            })
+    } catch (error) {
+        throw error;
+    }
+}
+
 //auth 컨트롤러에서 구현한 함수를 객체로 맵핑함
 module.exports.login = async (req, res, next) => {
     try {
         //이제 이 부분을 React Native의 body를 가져오면 됨
-        const [inputEmail, inputPW] = req.body;
+        const inputEmail = req.body.email;
+        const inputPW = req.body.password;
 
-        const query = `SELECT * FROM users WHERE email = ${inputEmail}`;
-        let conn = await mariaDB.getConnection();
-        await conn.query(query)
+        let sql = `SELECT * FROM users WHERE email = '${inputEmail}' AND password = '${hash(inputPW)}'`;
+        await conn.query(sql)
             .then(row => {
-                let hashPW = hash(inputPW); //비번 asdf
-                if(row[0].password == hashPW){
+                if(row.length == 0) {
+                    return res.json({fail: '로그인 정보 없음', type: typeof row[0]})
+                } else {
                     return res.json(row[0]);
                 }
-            }).catch(() => {
-                return res.json({fail: 'i dont...know ㅠㅠ'});
+            }).catch((err) => {
+                return res.json({fail: err});
             })
     } catch(err) {
+        throw err;
         console.log(`로그인 컨트롤러 에러 : ${err}`);
     }
 };
