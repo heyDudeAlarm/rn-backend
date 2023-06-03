cc00748466a12c079a382116d84e90a51ebb1d88
HA JIN <w2128@e-mirim.hs.kr>
2023-06-03 20:27:23 +0900

    /alarm 라우터 등록

--- Backend/server.js
+++ Backend/server.js
@@ -6,28 +6,20 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const app = express();
 dotenv.config();
 
-// const { sequelize } = require('./database/database');
 const authRouter = require('./routes/auth');
-
-const app = express();
+const alarmRouter = require('./routes/alarm');
 
 app.set('port', process.env.port);
-
-// sequelize.sync({ force: false })
-//     .then(() => {
-//         console.log('데이터베이스 연결 성공');
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//     });
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/auth', authRouter);//로그인, 회원가입, 로그아웃
+app.use('/alarm', alarmRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
