368bd7bd2e9513ac30a6522f346fd298c9bd65fb
HA JIN <w2128@e-mirim.hs.kr>
2023-06-04 21:02:48 +0900

    user_id에 해당하는 알람 정보를 가져오는 컨트롤러 작성

--- Backend/controllers/alarm.js
+++ Backend/controllers/alarm.js
@@ -1,3 +1,11 @@
 const mariaDB = require('../database/config');
+const Alarm = require('../models/alarm');
 
-module.exports
+module.exports.showAlarm = async (req, res) => {
+    try {
+        const alarmData = await Alarm.findById(1); //1: user_id
+        res.json(alarmData);
+    } catch (err) {
+        res.json(err);
+    }
+}
