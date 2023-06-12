7283d1cff611613a787ce2248a2a52673a335265
HA JIN <w2128@e-mirim.hs.kr>
2023-06-12 22:42:54 +0900

    Audio 모델 설계 시작

---
+++ Backend/models/Audio.js
@@ -0,0 +1,9 @@
+const connection = require('../database/config');
+const Audio = {};
+
+//녹음파일 url, 메세지 저장하기 
+Audio.saveAudio = ({data}) => {
+    
+}
+
+module.exports = Audio;
--- Backend/models/record.js
+++
@@ -1,6 +0,0 @@
-const connection = require('../database/config');
-const Record = {};
-
-// Record.
-
-// module.exports = Record;
