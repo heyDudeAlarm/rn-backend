195f643a29f1716dcbc569291993cf725c145901
hajin <w2128@e-mirim.hs.kr>
2023-06-15 19:09:05 +0900

    Firebase Admin SDK

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const morgan  = require('morgan');
-const path = require('path');
-const session = require('express-session');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 dotenv.config();
+
+// Firebase Admin SDK
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./reactnativeheydude-firebase-adminsdk-w1p6w-1f5fb7587f.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const authRouter = require('./routes/auth');
 const alarmRouter = require('./routes/alarm');
@@ -23,7 +29,7 @@ app.use(bodyParser.json());
 app.use('/auth', authRouter);//로그인, 회원가입, 로그아웃
 app.use('/alarm', alarmRouter);
 app.use('/upload', uploadRouter);
-app.use('/friend', friendRouter);
+app.use('/friend', friendRouter); //친구
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
