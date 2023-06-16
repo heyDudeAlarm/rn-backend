49171f12f5686afa6a1734df0a2d009ab1b53008
hajin <w2128@e-mirim.hs.kr>
2023-06-16 19:28:03 +0900

    server.js -> session 추가

--- Backend/server.js
+++ Backend/server.js
@@ -2,22 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const crypto = require('crypto');
 const app = express();
 dotenv.config();
-
-// Firebase Admin SDK
-const admin = require("firebase-admin");
-
-var serviceAccount = require("./reactnativeheydude-firebase-adminsdk-w1p6w-1f5fb7587f.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 const authRouter = require('./routes/auth');
 const alarmRouter = require('./routes/alarm');
 const uploadRouter = require('./routes/upload');
+const downloadRouter = require('./routes/download');
 const friendRouter = require('./routes/friend');
+
+const generateSecret = () => {
+  return crypto.randomBytes(32).toString('hex');
+};
 
 app.set('port', process.env.port);
 
@@ -25,6 +23,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+app.use(session({
+  secret: generateSecret(),
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/auth', authRouter);//로그인, 회원가입, 로그아웃
 app.use('/alarm', alarmRouter);
