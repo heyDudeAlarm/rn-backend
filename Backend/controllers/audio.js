25dc875d35f1493f580b1534bdf9e560dee09f75
hajin <w2128@e-mirim.hs.kr>
2023-06-16 19:29:06 +0900

    컨트롤러 오디오, 유저, 푸시알림

---
+++ Backend/controllers/audio.js
@@ -0,0 +1,60 @@
+const multer = require("multer");
+const { Upload } = require("@aws-sdk/lib-storage");
+const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+
+const Audio = require('../models/Audio');
+const upload = multer({ storage: multer.memoryStorage() });
+
+// API KEY 보관법
+const client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+// /audio/upload/:id
+module.exports.upload = (upload.single("file"), async (req, res, next) => {
+    //녹음 파일
+    const file = req.file;
+    //녹음 파일을 받는 유저
+    // const receivedUser = req.body.userID;
+    //녹음 메세지
+    const message = req.body.message;
+    //녹음을 한 유저
+    const sendUser = req.params.id;
+
+    //AWS s3 버킷에 파일 업로드
+    const upload = new Upload({
+      client: client,
+      params: {
+        Bucket: process.env.AWS_BUCKET,
+        Key: `audio/audio_${sendUser}.mp3`,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      },
+    });
+
+    //업로드 성공하면 상대 유저에게 푸쉬알림(파일다운로드) 전송
+    //-> 푸쉬알림 /push/res_record
+    upload
+      .done()
+      .then((res) => {
+        let uploadDate = new Date().toLocaleString();
+
+        const data = {
+          send_user: sendUser,
+          audio_url: res.Location,
+          audio_msg: message,
+          audio_date: uploadDate,
+          received_user: receivedUser,
+        };
+        console.log(data);
+        Audio.insertData(data); //DB에 알람정보 저장
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.json({ fail: err });
+      });
+})
--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -5,27 +5,17 @@ const Auth = require('../models/Auth');
 //회원가입 컨트롤러
 module.exports.join = async (req, res, next) => {
     try {
-        let conn = await mariaDB.getConnection();
         const email = req.body.email;
         const password = req.body.password;
         const nickname = req.body.nickname;
 
-        let sql = `INSERT INTO users(email, nickname, password) VALUES(
-            '${email}', '${nickname}', '${Auth.hash(password)}'
-           );`;
-        await conn.query(`SELECT * FROM users WHERE email = '${email}'`)
-            .then(row => {
-                if(row == 0){
-                    conn.query(sql)
-                        .then(row => {
-                            res.end('회원가입 성공');
-                    })
-                } else {
-                    res.json({fail: '계정이 이미 존재함'});
-                }
-            }).catch(err => {
-                console.log(err);
-            })
+        const userData = {
+            email: email,
+            password: password,
+            nickname: nickname,
+        }
+        
+        const result = await Auth.addUser(userData);
     } catch (error) {
         throw error;
     }
@@ -42,6 +32,16 @@ module.exports.login = async (req, res, next) => {
         res.json(user[0])
     } catch(err) {
         throw err;
-        console.log(`로그인 컨트롤러 에러 : ${err}`);
     }
 };
+
+module.exports.getToken = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const token = await Auth.getToken(userId);
+
+        res.end(token);
+    } catch (error) {
+        
+    }
+}
