6b472b6c69587d0bf6713a6ccb223451e35132fb
hajin <w2128@e-mirim.hs.kr>
2023-06-16 20:46:57 +0900

    로그인 세션 인증 미들웨어

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,20 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../controllers/auth');
-
-const loginRequired = (req, res, next) => {
-    if(req.session.user) {
-        next()
-    } else {
-        res.status(401).json({ message: "login required" })
-    }
-}
+const authenticate = require('../middleware/authentication');
 
 // /auth/...
 router.post('/join', auth.join);
 router.post("/login", auth.login);
-router.get("/me", loginRequired, auth.me);
-router.get("/device_token", auth.getToken);
+// router.get("/me", authenticate, auth.me);
+router.get("/device_token", authenticate ,auth.getToken);
 
 // put : 토큰 재저장은 나중에
 module.exports = router;
